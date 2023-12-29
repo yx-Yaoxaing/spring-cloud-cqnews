55de5ecb28c899ffaef15fd0e037e894f8861e19
‘yaoxiang <yaoxiang@cqliving.com>
2023-12-29 11:22:28 +0800

    微信

--- spring-cloud-cqnews-starter-parent/wechat-spring-boot-starter/src/main/java/com/cqnews/cloud/wechat/WechatTemplate.java
+++ spring-cloud-cqnews-starter-parent/wechat-spring-boot-starter/src/main/java/com/cqnews/cloud/wechat/WechatTemplate.java
@@ -2,9 +2,8 @@ package com.cqnews.cloud.wechat;
 
 /**
  * @author uYxUuu
+ * @version 0.1.0
  */
 public class WechatTemplate {
-
-
 
 }
---
+++ spring-cloud-cqnews-starter-parent/wechat-spring-boot-starter/src/main/java/com/cqnews/cloud/wechat/config/WechatProperties.java
@@ -0,0 +1,15 @@
+package com.cqnews.cloud.wechat.config;
+
+
+/**
+ * @author uYxUuu
+ * @version 0.1.0
+ */
+public class WechatProperties {
+
+    private String appId;
+
+    private String secret;
+
+
+}
